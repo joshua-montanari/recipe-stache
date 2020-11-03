965cc35f62974f6d2ec5c2309b88fb084d7d3dfa
joshua-montanari <joshua.montanari@gmail.com>
2020-11-03 10:15:22 -0500

    File structure and user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: true, minlength: 5},
+    username: {type: String},
+    recipeCount: {type: Number, default: 0},
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
