a0fb374c4c1dd3efbd1f684531a551d282217475
Derek Hassan <dhass421@gmail.com>
2020-11-30 16:28:16 -0500

    Added file upload button

--- Client/src/Pages/CreateRecipe/CreateRecipe.js
+++ Client/src/Pages/CreateRecipe/CreateRecipe.js
@@ -16,6 +16,7 @@ const CreateRecipe = () => {
     const [category, setCategory] = useState('');
     const [ingredientText, setIngredientText] = useState('');
     const [ingredients, setIngredients] = useState([]);
+    const [image, setImage] = useState('');
 
     const addIngredient = () => {
         if(!ingredientText.length) { return; }
@@ -27,20 +28,34 @@ const CreateRecipe = () => {
 
         if(!recipeName || !recipeSteps || !category || !ingredients.length) { return; }
 
-        const newRecipe = {
-            name: recipeName,
-            steps: recipeSteps,
-            category: category,
-            ingredients: ingredients,
-            createdBy: localStorage.getItem('user-id'),
-        }
+
+        const formData = new FormData();
+        formData.append('name', recipeName);
+        formData.append('steps', recipeSteps);
+        formData.append('category', category);
+        formData.append('ingredients', ingredients);
+        formData.append('createdBy', localStorage.getItem('user-id'));
+        formData.append('file', image[0]);
+
+
+        // const newRecipe = {
+        //     name: recipeName,
+        //     steps: recipeSteps,
+        //     category: category,
+        //     ingredients: ingredients,
+        //     createdBy: localStorage.getItem('user-id'),
+        //     file: image
+        // }
+
+        console.log(localStorage.getItem('auth-token'));
 
         const headers = {
-            Accept: 'application/json'
+            'Content-Type': 'multipart/form-data',
+            'x-auth-token': localStorage.getItem('auth-token'),
         }
 
         try{
-            const response = await axios.post('http://localhost:5000/recipes', newRecipe, headers)
+            const response = await axios.post('http://localhost:5000/recipes', formData, headers)
             console.log(response);
         } catch(e) {
             console.log(e);
@@ -55,9 +70,9 @@ const CreateRecipe = () => {
             <form>
                 <TextField id="recipe-name" label="Recipe Name" variant="filled" onChange={e => setRecipeName(e.target.value)} />
                 {/* TODO: Add Markdown editor */}
-                {/* <TextField id="recipe-steps" label="Recipe Steps" variant="filled" multiline rows={4} onChange={e => setRecipeSteps(e.target.value)} /> */}
+                <TextField id="recipe-steps" label="Recipe Steps" variant="filled" multiline rows={4} onChange={e => setRecipeSteps(e.target.value)} />
 
-                <RecipeEditor recipeSteps={recipeSteps} setRecipeSteps={setRecipeSteps} />
+                {/* <RecipeEditor recipeSteps={recipeSteps} setRecipeSteps={setRecipeSteps} /> */}
                 
                 <Select
                 labelId="category"
@@ -79,6 +94,17 @@ const CreateRecipe = () => {
                 <TextField id="ingredients" label="Ingredients" variant="filled" value={ingredientText} onChange={e => setIngredientText(e.target.value)} />
 
                 <Button onClick={addIngredient}>Add</Button>
+
+                <input
+                    accept="image/*"
+                    id="raised-button-file"
+                    type="file"
+                    style={{ display: 'none' }}
+                    onChange={e => setImage(e.target.files)}
+                />
+                <label htmlFor="raised-button-file">
+                    <Button variant="raised" component="span">Upload Image</Button>
+                </label>
 
                 <Button variant="contained" color="primary" onClick={submitForm}>Submit</Button>
             </form>
